5f06c03c8d91c092f245633dd7a02932b3634bed
tklipp8 <turnerklippel@gmail.com>
2025-01-14 17:38:19 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Assignment_2_Dice_Rolls;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine("");
+        Console.Write("How many dice rolls would you like to simulate? ");
+
+        int numRolls = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("");
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine("Total number of rolls = " + numRolls);
+        Console.WriteLine("");
+
+        diceRoller dice = new diceRoller();
+        int[] rollResults = dice.RollDice(numRolls);
+
+        for (int i = 2; i <= 12; i++)
+        {
+            int rollResult = rollResults[i];
+            double v = ((double)rollResults[i] / numRolls) * 100;
+            int percentRolled = (int)v;
+            Console.WriteLine($"{i}: {new string('*', percentRolled)}");
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+}
---
+++ diceRoller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2_Dice_Rolls
+{
+    class diceRoller
+    {
+        Random rnd = new Random();
+
+        public int[] RollDice(int numRolls)
+        {
+            int[] rollResults = new int[13];
+
+            for (int i = 0; i < numRolls; i++)
+            {
+                int diceOne = rnd.Next(1, 7);
+                int diceTwo = rnd.Next(1, 7);
+                int diceCombo = diceOne + diceTwo;
+
+                rollResults[diceCombo]++;
+            }
+            return rollResults;
+        }
+    }
+}
